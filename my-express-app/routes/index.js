4e7c2389f2e0e3e2d07f1612ba39d168e2d8b7fe
melecouvreur <112541602+melecouvreur@users.noreply.github.com>
2023-02-15 16:55:22 +0000

    user get route

--- my-express-app/routes/index.js
+++ my-express-app/routes/index.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const db = require("../model/helper");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send({ title: 'Express' });
 });
+
+router.get("/users", function(req, res, next) {
+  db("SELECT * FROM user_info;")
+    .then(results => {
+      res.send(results.data);
+    })
+    .catch(err => res.status(500).send(err));
+});
+
 
 module.exports = router;
 
